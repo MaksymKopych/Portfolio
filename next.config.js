b3a52881ba311e00e0822dd956a78c196420d04b
MaksymKopych <maximkopych@gmail.com>
2023-03-31 16:34:11 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+};
+const basePath = "/Portfolio";
+module.exports = {
+  basePath,
+};
+module.exports = nextConfig;
