b93639cdf269d8d1bbabdf94032dc8b522fae3e4
MaksymKopych <maximkopych@gmail.com>
2023-04-07 12:40:41 -0400

    fix meil

--- pages/_document.js
+++ pages/_document.js
@@ -11,15 +11,11 @@ const document = () => {
           to contribute additional success to your organisation."
           id="description"
         ></meta>
-        <meta
-          content="https://instagram.fyzd1-3.fna.fbcdn.net/v/t51.2885-15/327767158_1208832109731819_2596162007852776560_n.jpg?stp=dst-jpg_e35_p320x320&_nc_ht=instagram.fyzd1-3.fna.fbcdn.net&_nc_cat=103&_nc_ohc=Dgi5rO09mz4AX9EO2MQ&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzAyNTAxODg3OTkzMjI1NDQxNQ%3D%3D.2-ccb7-5&oh=00_AfBe724gax6-ySslwgbN2xLyfzKWdOZFFNxLoEB53tEOYw&oe=64348ED8&_nc_sid=1527a3"
-          property="og:image"
-          name="og:image"
-        ></meta>
+        <meta content="/meta.jpeg" property="og:image" name="og:image"></meta>
         <meta
           name="og:image"
           itemprop="image"
-          content="https://instagram.fyzd1-3.fna.fbcdn.net/v/t51.2885-15/327767158_1208832109731819_2596162007852776560_n.jpg?stp=dst-jpg_e35_p320x320&_nc_ht=instagram.fyzd1-3.fna.fbcdn.net&_nc_cat=103&_nc_ohc=Dgi5rO09mz4AX9EO2MQ&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzAyNTAxODg3OTkzMjI1NDQxNQ%3D%3D.2-ccb7-5&oh=00_AfBe724gax6-ySslwgbN2xLyfzKWdOZFFNxLoEB53tEOYw&oe=64348ED8&_nc_sid=1527a3"
+          content="/meta.jpeg"
           property="og:image"
         ></meta>
       </Head>
