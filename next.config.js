6af6c41861ca45f3d3ebf6976a7e9c8a6a437f14
MaksymKopych <maximkopych@gmail.com>
2023-04-11 12:05:27 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  async redirects() {
-    return [
-      {
-        source: "https://maksymkopych.github.io/Portfolio/",
-        destination: "https://portfolio.maksymkopych.vercel.app/",
-        permanent: true,
-      },
-    ];
-  },
 };
 module.exports = nextConfig;
